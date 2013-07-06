73bdef4fc5d6999b7c35e4eda6199374f3330bea
jagajaga <senia00@gmail.com>
2013-07-06 11:34:31 +0400

    wallpaper chang script

---
+++ myscripts/wallpaper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+shopt -s nullglob
+cd ~/.wallpaper
+
+while true; do
+	files=()
+	for i in *.jpg *.png; do
+		[[ -f $i ]] && files+=("$i")
+	done
+	range=${#files[@]}
+
+	((range)) && feh --bg-scale "${files[RANDOM % range]}"
+
+	sleep 15m
+done
