308adcaa59e466cd2f648c0f4e80c31919014a46
jagajaga <senia00@gmail.com>
2013-08-15 15:05:20 +0400

    fix grep percentage

--- myscripts/low_battery.sh
+++ myscripts/low_battery.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # notify if battery is low
 
-battery_float=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | cut -d ':' -f 2 | sed -e 's/^[ \t]*//' | cut -d '%' -f 1)
-battery_state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | cut -d ':' -f 2 | sed -e 's/^[ \t]*//' | cut -d '%' -f 1)
-battery_int=$(echo "($battery_float+0.5)/1" | bc )
+battery_int=$(cat /sys/class/power_supply/BAT0/capacity)
+battery_state=$(cat /sys/class/power_supply/BAT0/status)
+#battery_int=$(echo "($battery_float+0.5)/1" | bc )
 if [ "$battery_int" -le "9" ]
 then
-    if [ "$battery_state" == "discharging" ]
+    if [ "$battery_state" == "Discharging" ]
     then
         zenity --info --title "Low battery" --text "Battery is $battery_int%. Enable charging!"
     fi
