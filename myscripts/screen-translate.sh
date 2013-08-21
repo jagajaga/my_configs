12297f0430ee507c1d90c90d8e0f0fddeee59047
jagajaga <senia00@gmail.com>
2013-08-21 14:34:21 +0400

    improved screen translate

--- myscripts/screen-translate.sh
+++ myscripts/screen-translate.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 # translate-selection.sh - google translation of selected text
 # needs xsel to read from clipboard
+google-translate() {
+    source=auto
+    target="$2"
+    result=$(curl -s -i --user-agent "" -d "sl=$source" -d "tl=$target" --data-urlencode "text=$1" http://translate.google.com)
+    encoding=$(awk '/Content-Type: .* charset=/ {sub(/^.*charset=["'\'']?/,""); sub(/[ "'\''].*$/,""); print}' <<<"$result")
+    #iconv -f $encoding <<<"$result" | awk 'BEGIN {RS="<div"};/<span[^>]* id=["'\'']?result_box["'\'']?/ {sub(/^.*id=["'\'']?result_box["'\'']?(>| [^>]*>)([ \n\t]*<[^>]*>)*/,"");sub(/<.*$/,"");print}' | html2text -utf8
+    iconv -f $encoding <<<"$result" |  awk 'BEGIN {RS="</div>"};/<span[^>]* id=["'\'']?result_box["'\'']?/' | html2text 
+}
 
 query=$(xsel)
 #notify-send "Google Translate" "Query is ${query}"
-translation=$(/home/jaga/myscripts/google-translate.sh "$query" "ru")
+translation=$(google-translate "$query" "ru")
 notify-send "$query → $translation"
 #zenity --info --title "Translation" --text "$translation"
 exit
