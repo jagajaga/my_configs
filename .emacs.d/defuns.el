99317eb6cf48ec548fa1af2f74983f75ae6948bb
jagajaga <senia00@gmail.com>
2013-11-28 18:00:37 +0400

    remove .emacs folder

--- .emacs.d/defuns.el
+++
@@ -1,9 +0,0 @@
-(defun indent-buffer ()
- "Indents an entire buffer using the default intenting scheme."
- (interactive)
- (point-to-register 'o)
- (delete-trailing-whitespace)
- (indent-region (point-min) (point-max) nil)
- (untabify (point-min) (point-max))
- (jump-to-register 'o))
-
--- .emacs.d/elpa/sr-speedbar-20130309.1959/sr-speedbar-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; sr-speedbar-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("sr-speedbar-pkg.el" "sr-speedbar.el")
-;;;;;;  (20823 24079 447361 803000))
-
-;;;***
-
-(provide 'sr-speedbar-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; sr-speedbar-autoloads.el ends here
