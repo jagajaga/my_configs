44b6646b3896a73e143a8dd5cfa50930cb9e1902
Arseniy Seroka <ars.seroka@gmail.com>
2014-02-26 16:13:59 +0400

    nixos haskell coworking

--- .nixpkgs/desktop_config.nix
+++ .nixpkgs/desktop_config.nix
@@ -1,39 +1,34 @@
- {
-   packageOverrides = pkgs : with pkgs; {
-      vimEnv = pkgs.buildEnv
+pkgs : {
+   packageOverrides = self : rec {
+      vimEnv = self.buildEnv
       { 
         name = "vim-env";
-        paths = with pkgs.vimPlugins;
+        paths = with self.vimPlugins;
           [ YouCompleteMe syntastic taglist tagbar vimproc ctags ];
       };
 
-      hsEnv = pkgs.buildEnv
-      { 
-        name = "hs-env";
-        paths = with pkgs.haskellPackages;
-          [ ghc haskellPlatform gtk Chart cairo gloss lens zlib alsaCore alsaPcm cabal2nix pandoc hakyll ghcMod unorderedContainers xmlConduit xmonad xmobar xmonadContrib AgdaExecutable haddock haskellSrcExts lushtags ];
-      };
+      hsEnv = self.haskellPackages.ghcWithPackagesOld (self : with self; [ ghc gtk Chart cairo gloss lens zlib alsaCore alsaPcm cabal2nix pandoc hakyll ghcMod unorderedContainers xmlConduit xmonad xmobar xmonadContrib AgdaExecutable haddock haskellSrcExts stylishHaskell aeson mtlparse regexpr ]);
+#lushtags 
 
-      developmentEnv = pkgs.buildEnv
+      developmentEnv = self.buildEnv
       {
         name = "development-env";
-        paths = with pkgs;
-        [ zlib freeglut bzip2 xlibs.libX11 mesa ];
+        paths = with self;
+        [ zlib freeglut bzip2 xlibs.libX11 mesa_drivers mesa_glu ];
       };
 
-      hugeEnv = pkgs.buildEnv
+      hugeEnv = self.buildEnv
       {
         name = "huge-env";
-        paths = with pkgs;
+        paths = with self;
         [ gimp libreoffice chromiumWrapper inkscape ];
       };
 
-      deEnv = pkgs.buildEnv
+      deEnv = self.buildEnv
       {
         name = "de-env";
-        paths = with pkgs;
-        [ skype dropbox haskellPackages.yeganesh dmenu trayer moc transmission_gtk transmission_remote_gtk firefoxWrapper tor p7zip unrar mc vlc imagemagick spaceFM bc darcs dropbox-cli evince file flac freetype gtkvnc hdparm iftop lastfmsubmitd mirage  steam steamChrootEnv unetbootin ];
+        paths = with self;
+        [ skype dropbox haskellPackages.yeganesh dmenu trayer moc transmission_gtk transmission_remote_gtk firefoxWrapper tor p7zip unrar mc vlc imagemagick spaceFM bc darcs dropbox-cli evince file flac freetype gtkvnc hdparm iftop lastfmsubmitd mirage steam steamChrootEnv unetbootin dwb ];
       };
-
-   };
- }
+  };
+}
