226d61f63664a718543dc1739a4715f558f9b371
Arseniy Seroka <ars.seroka@gmail.com>
2014-11-14 00:16:36 +0300

    move to nix based vim plugins configs

--- .nixpkgs/desktop_config.nix
+++ .nixpkgs/desktop_config.nix
@@ -6,38 +6,51 @@
         vimrcConfig = {
         vam.knownPlugins = pkgs.vimPlugins; # optional
         vam.pluginDictionaries = [
-            {names = [ 
+            { names = [ 
+                "ghcmod" 
+                "haskellconceal"
+                "hoogle"
+                "lushtags"
+                "neco-ghc"
+              ]; ft_regex = "^hs\$"; 
+            }
+            { names = [ 
+                "rust"
+              ]; ft_regex = "^rs\$"; 
+            }
+            { names = [ 
+                "vim-addon-nix"
+              ]; ft_regex = "^nix\$"; 
+            }
+            { names = [ 
+                "idris-vim"
+              ]; ft_regex = "^idr\$"; 
+            }
+            { names = [ 
                 "a"
                 "airline"
                 "calendar"
                 "colors-solarized"
+                "ctrlp"
                 "easy-align"
                 "easymotion"
                 "fugitive"
-                "ghcmod"
                 "gist-vim"
                 "gitgutter"
                 "hardtime"
-                "haskellconceal"
                 "hier"
-                "hoogle"
-                "idris-vim"
                 "latex-box"
                 "latex-live-preview"
-                "lushtags"
-                "neco-ghc"
                 "nerdcommenter"
                 "nerdtree"
                 "quickfixstatus"
                 "quickrun"
                 "rainbow_parentheses"
-                "rust"
                 "shabadou"
                 "signature"
                 "surround"
                 "table-mode"
                 "tabmerge"
-                "tagbar"
                 "tagbar"
                 "taglist"
                 "thumbnail"
@@ -53,7 +66,96 @@
         ];
 
         customRC = ''
+            set history=700
+            set confirm
+            set cindent
+            set t_Co=256
+
+            autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
+
             set hidden
+            vmap <Enter> <Plug>(EasyAlign)
+            nmap s <Plug>(easymotion-s2)
+            map  / <Plug>(easymotion-sn)
+            omap / <Plug>(easymotion-tn)
+            map  n <Plug>(easymotion-next)
+            map  N <Plug>(easymotion-prev)
+
+            let g:calendar_google_calendar = 1
+            let g:calendar_google_task = 1
+
+            let g:gist_clip_command = 'xclip -selection clipboard'
+            let g:gist_detect_filetype = 1
+            let g:gist_use_password_in_gitconfig = 1
+
+            let NERDTreeShowHidden=1
+            let NERDTreeQuitOnOpen=1
+            let g:necoghc_enable_detailed_browse = 1
+
+            let g:YUNOcommit_after = 20
+
+            au VimEnter * RainbowParenthesesToggle
+            au Syntax * RainbowParenthesesLoadRound
+            au Syntax * RainbowParenthesesLoadSquare
+            au Syntax * RainbowParenthesesLoadBraces
+
+            let g:airline_enable_branch=1
+            let g:airline_enable_syntastic=1
+            let g:airline_theme='dark'
+            let g:airline_left_sep = '▶'
+            let g:airline_right_sep = '◀'
+            let g:airline_linecolumn_prefix = '¶ '
+            let g:airline_branch_prefix = '⎇ '
+            let g:airline_paste_symbol = 'ρ'
+            let g:airline_detect_modified=1
+            let g:airline_detect_paste=1
+
+            " How many lines should be searched for context
+            let g:hasksyn_indent_search_backward = 100
+
+            " Should we try to de-indent after a return
+            let g:hasksyn_dedent_after_return = 1
+
+            " Should we try to de-indent after a catchall case in a case .. of
+            let g:hasksyn_dedent_after_catchall_case = 1
+
+            let tagbar_autofocus=1
+            let tagbar_autoclose=1
+
+            let g:ycm_global_ycm_extra_conf = '~/.vim/bundle/YouCompleteMe/cpp/ycm/.ycm_extra_conf.py'
+            autocmd FileType c let g:ycm_global_ycm_extra_conf = '~/.vim/bundle/YouCompleteMe/c/ycm/.ycm_extra_conf.py'
+            let g:ycm_semantic_triggers =  {
+            \   'c' : ['->', '.'],
+            \   'cpp' : ['->', '.', '::'],
+            \   'objc' : ['->', '.'],
+            \   'ocaml' : ['.', '#'],
+            \   'cpp,objcpp' : ['->', '.', '::'],
+            \   'perl' : ['->'],
+            \   'php' : ['->', '::'],
+            \   'cs,java,javascript,d,vim,python,perl6,scala,vb,elixir,go' : ['.'],
+            \   'ruby' : ['.', '::'],
+            \   'lua' : ['.', ':'],
+            \   'erlang' : [':'],
+            \ }
+            nnoremap <C-E> <ESC>:YcmCompleter GoToDefinitionElseDeclaration<CR>
+            let g:ctrlp_map = '<c-p>'
+            let g:ctrlp_cmd = 'CtrlP'
+            let g:ctrlp_working_path_mode = 'ra'
+            set wildignore+=*/tmp/*,*.so,*.swp,*.zip     " MacOSX/Linux
+
+            autocmd BufRead,BufNewFile ~/.xmonad/* call s:add_xmonad_path()
+            function! s:add_xmonad_path()
+            if !exists('b:ghcmod_ghc_options')
+                let b:ghcmod_ghc_options = []
+            endif
+            call add(b:ghcmod_ghc_options, '-i' . expand('~/.xmonad/lib'))
+            endfunction
+
+            let g:solarized_termcolors=256
+            colorscheme solarized "desert
+            "colorscheme desert
+            set background=dark
+
             exec 'source '.fnameescape($HOME.'/.vimrc')
         '';
         };
