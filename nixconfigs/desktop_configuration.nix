a754966688e8a5ca5684835d495621fba117cc21
Arseniy Seroka <ars.seroka@gmail.com>
2014-11-19 15:20:31 +0300

    refactor configuration of nix

--- nixconfigs/desktop_configuration.nix
+++ nixconfigs/desktop_configuration.nix
@@ -3,60 +3,65 @@
 # or the NixOS manual available on virtual console 8 (Alt+F8).
 
 { config, pkgs, ... }:
-let 
-
-  priv = import (./private.nix);
-
+let
+  literals = import ./literals.nix {pkgs = pkgs;
+                ca   = pkgs.writeText "ca.crt" (builtins.readFile /root/.vpn/ca.crt);
+                cert = pkgs.writeText "alice" (builtins.readFile /root/.vpn/alice.crt);
+                key  = pkgs.writeText "alice.key" (builtins.readFile /root/.vpn/alice.key);  };
 in
 
 {
-  imports = [ <nixos/modules/programs/virtualbox.nix> ];
-  require =
-    [ 
+  imports = [
+    <nixos/modules/programs/virtualbox.nix>
+  ];
+  require = [
       ./desktop_hardware-configuration.nix
       ./private.nix
-    ];
+  ];
 
-
-  boot = { 
+  boot = {
     kernelPackages      = pkgs.linuxPackages_3_17;
-    loader.grub.timeout = 1;
     extraModprobeConfig = ''
       options snd slots=snd_usb_audio,snd-hda-intel
     '';
-    loader.grub.enable  = true;
-    loader.grub.version = 2;
-    loader.grub.device  = "/dev/sdc";
+    loader.grub = {
+      timeout = 1;
+      enable  = true;
+      version = 2;
+      device  = "/dev/sdc";
+    };
   };
 
   nix = {
     package             = pkgs.nixUnstable;
     binaryCaches        = [ http://cache.nixos.org ];
     trustedBinaryCaches = [ http://cache.nixos.org ];
-    useChroot = true;
+    useChroot           = true;
     gc = {
       automatic = true;
       dates     = "2 weeks";
     };
   };
-  nixpkgs.config.allowUnfree = true;
-
-  networking = {
-    firewall.allowedUDPPorts = [ 7777 ];
-    hostName = "nixos"; 
-    connman.enable = true;
-    extraHosts = ''
-        fc5d:baa5:61fc:6ffd:9554:67f0:e290:7535 nodeinfo.hype
-        fcbf:7bbc:32e4:716:bd00:e936:c927:fc14 socialno.de
-        fcd5:76e1:c1c2:e946:b266:8543:c1d5:67ac hypeoverflow.com
-    '';
+  nixpkgs.config = {
+    allowUnfree             = true;
+    virtualbox.enableExtensionPack = true;
   };
 
-  fileSystems."/home" =
-    { device          = "/dev/sdc2";
-      fsType          = "ext4";
-      options         = "data=journal,users,rw,user,auto,exec";
+  networking = {
+    firewall = {
+      allowedUDPPorts = [ 7777 ];
+      allowedTCPPorts = [ 7777 ];
     };
+    hostName                 = "nixos";
+    extraHosts               = literals.extraHosts;
+    connman.enable           = true;
+  };
+
+  fileSystems."/home" = {
+    device          = "/dev/sdc2";
+    fsType          = "ext4";
+    options         = "data=journal,users,rw,user,auto,exec";
+  };
 
   i18n = {
     consoleFont   = "lat9w-16";
@@ -64,87 +69,34 @@ in
     defaultLocale = "en_US.UTF-8";
   };
 
-  security.sudo.configFile = ''
-    Cmnd_Alias SUSPEND = /var/run/current-system/sw/sbin/pm-suspend, /var/run/current-system/sw/bin/systemctl suspend
+  hardware = {
+    opengl = {
+      driSupport32Bit = true;
+    };
+    bluetooth.enable = true;
+  };
 
-    %users      ALL=NOPASSWD: SUSPEND
-  '';
+  security.sudo.configFile = literals.sudoConf;
 
-  services.dbus.enable            = true;
-  services.nixosManual.showManual = true;
-  services.locate.enable          = true;
-  services.udisks2.enable         = true;
-  services.openssh.enable         = true;
-  services.printing.enable        = true;
-  /*services.tor.client.enable = true;*/
-  services.mysql.enable           = true;
-  services.mysql.package          = pkgs.mysql;
-  services.ntp.enable             = true;
-
-
-  services.openvpn = {
-    enable = true;
-    servers = 
-    {
-        client = 
-            let 
-                ca = pkgs.writeText "ca.crt" (builtins.readFile /root/.vpn/ca.crt);
-                cert = pkgs.writeText "alice" (builtins.readFile /root/.vpn/alice.crt);
-                key = pkgs.writeText "alice.key" (builtins.readFile /root/.vpn/alice.key);
-            in
-            {
-                config = ''
-                    client
-                    nobind
-                    dev tun
-                    redirect-gateway def1
-                    ca ${ca}
-                    cert ${cert}
-                    key ${key}
-                    <dh>
-                    -----BEGIN DH PARAMETERS-----
-                    MEYCQQCQn3sGfqQWQtMH4GNWHeZG6210sE3cssDGitRfv9T9knp00zIquPI3tuRa
-                    xywN8CG+Ww/V8kBIgLBfqRqnThdzAgEC
-                    -----END DH PARAMETERS-----
-                    </dh>
-
-                    <connection>
-                    remote 178.62.202.50 1194 udp
-                    </connection>
-
-                    <connection>
-                    remote 178.62.202.50 443 tcp-client
-                    </connection>
-                '';
-            up = "echo nameserver $nameserver | ''${pkgs.openresolv}/sbin/resolvconf -m 0 -a $dev";
-            down = "''${pkgs.openresolv}/sbin/resolvconf -d $dev";
-          };
+  services = {
+    dbus.enable            = true;
+    nixosManual.showManual = true;
+    locate.enable          = true;
+    udisks2.enable         = true;
+    openssh.enable         = true;
+    printing.enable        = true;
+    ntp.enable             = true;
+    mysql = {
+      enable  = true;
+      package = pkgs.mysql;
+    };
+    openvpn = {
+      enable         = true;
+      servers.client = literals.openVPNConf;
     };
   };
-
+  /*services.tor.client.enable = true;*/
   /*services.cjdns.enable = true;*/
-
-  sound.extraConfig = ''
-    defaults.pcm.!card 3
-  '';
-  
-
-
-  users.extraUsers.jaga = {
-    description = "";
-    createHome = true;
-    home = "/home/jaga";
-    group = "users";
-    extraGroups = [ "wheel" "networkmanager" "adb" "video" "power" ];
-    shell = "${pkgs.zsh}/bin/zsh";
-    uid = 1000;
-  };
-
-  hardware.opengl = {
-    driSupport32Bit = true;
-  };
-
-  hardware.bluetooth.enable = true;
 
   services.xserver = {
     exportConfiguration = true;
@@ -156,54 +108,58 @@ in
     displayManager.slim = {
       enable = true;
       autoLogin = false;
-      defaultUser = "jaga"; 
+      defaultUser = "jaga";
       theme = pkgs.fetchurl {
-          url = https://github.com/jagajaga/nixos-slim-theme/archive/1.1.tar.gz;
+          url    = https://github.com/jagajaga/nixos-slim-theme/archive/1.1.tar.gz;
           sha256 = "66c3020a6716130a20c3898567339b990fbd7888a3b7bbcb688f6544d1c05c31";
         };
-    }; 
-    desktopManager.default = "none";
-    desktopManager.xterm.enable = false;
-    windowManager.default = "xmonad";
-    windowManager.xmonad.enable = true;
-    config = ''
-        Section "InputClass"
-            Identifier   "Kensington Slimblade Trackball"
-            MatchProduct "Kensington Kensington Slimblade Trackball"
-            Option       "ButtonMapping" "1 8 3 4 5 6 7 2 9 10 11 12"
-            Option       "EmulateWheel"       "True"
-            Option       "EmulateWheelButton" "8"
-            Option       "XAxisMapping"       "6 7"
-            Option       "ZAxisMapping" "4 5"
-            Option       "EmulateWheelInertia" "75"
-        EndSection
-    '';
+    };
+    desktopManager = {
+      default = "none";
+      xterm.enable = false;
+    };
+    windowManager = {
+      default = "xmonad";
+      xmonad.enable = true;
+    };
+    config = literals.trackBallConf;
+  };
+
+  sound.extraConfig = literals.alsaConf;
+
+  users.extraUsers.jaga = {
+    description = "Arseniy Seroka";
+    createHome  = true;
+    home        = "/home/jaga";
+    group       = "users";
+    extraGroups = [ "wheel" "networkmanager" "adb" "video" "power" "vboxusers" ];
+    shell       = "${pkgs.zsh}/bin/zsh";
+    uid         = 1000;
   };
 
   time.timeZone = "Europe/Moscow";
 
   environment.systemPackages = with pkgs; [
-   zsh
    bash
    htop
    iotop
+   zsh
 
    pmutils
    wget
 
-   gcc
    automake
-   pkgconfig
+   clang
+   cmake
+   gcc
+   git
    gnumake
    jdk
-   clang
-   subversion
-   git
+   pkgconfig
+   python
    python27
    python33
-   python
-   cmake
-
+   subversion
    androidsdk_4_4
    stdenv
    dejavu_fonts
@@ -218,7 +174,7 @@ in
 
   ];
   fonts = {
-    enableFontDir = true;
+    enableFontDir          = true;
     enableGhostscriptFonts = true;
     fonts = [
        pkgs.corefonts
@@ -230,5 +186,4 @@ in
        pkgs.ttf_bitstream_vera
     ];
   };
-
 }
---
+++ nixconfigs/literals.nix
@@ -0,0 +1,56 @@
+{pkgs, ca, cert, key}:
+{
+  openVPNConf = { 
+      config = ''
+            client
+            nobind
+            dev tun
+            redirect-gateway def1
+            ca ${ca}
+            cert ${cert}
+            key ${key}
+            <dh>
+            -----BEGIN DH PARAMETERS-----
+            MEYCQQCQn3sGfqQWQtMH4GNWHeZG6210sE3cssDGitRfv9T9knp00zIquPI3tuRa
+            xywN8CG+Ww/V8kBIgLBfqRqnThdzAgEC
+            -----END DH PARAMETERS-----
+            </dh>
+
+            <connection>
+            remote 178.62.202.50 1194 udp
+            </connection>
+
+            <connection>
+            remote 178.62.202.50 443 tcp-client
+            </connection>
+    '';
+    up = "echo nameserver $nameserver | ''${pkgs.openresolv}/sbin/resolvconf -m 0 -a $dev";
+    down = "''${pkgs.openresolv}/sbin/resolvconf -d $dev";
+  };
+  trackBallConf = ''
+        Section "InputClass"
+            Identifier   "Kensington Slimblade Trackball"
+            MatchProduct "Kensington Kensington Slimblade Trackball"
+            Option       "ButtonMapping" "1 8 3 4 5 6 7 2 9 10 11 12"
+            Option       "EmulateWheel"       "True"
+            Option       "EmulateWheelButton" "8"
+            Option       "XAxisMapping"       "6 7"
+            Option       "ZAxisMapping" "4 5"
+            Option       "EmulateWheelInertia" "75"
+        EndSection
+    '';
+  extraHosts = ''
+        fc5d:baa5:61fc:6ffd:9554:67f0:e290:7535 nodeinfo.hype
+        fcbf:7bbc:32e4:716:bd00:e936:c927:fc14 socialno.de
+        fcd5:76e1:c1c2:e946:b266:8543:c1d5:67ac hypeoverflow.com
+    '';
+  sudoConf = ''
+    Cmnd_Alias SUSPEND = /var/run/current-system/sw/sbin/pm-suspend, /var/run/current-system/sw/bin/systemctl suspend
+
+    %users      ALL=NOPASSWD: SUSPEND
+  '';
+  alsaConf = ''
+    defaults.pcm.!card 3
+  '';
+
+}
