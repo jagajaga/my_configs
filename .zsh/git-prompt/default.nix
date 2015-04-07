91d890a6979a2b99336dbc8e48b9a3afe2fbe528
Arseniy Seroka <ars.seroka@gmail.com>
2015-04-08 01:45:14 +0300

    git-prompt: update

--- .zsh/git-prompt/default.nix
+++ .zsh/git-prompt/default.nix
@@ -1,2 +1,2 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.haskellPackages.callPackage ./git-prompt.nix {}
+pkgs.haskellngPackages.callPackage ./git-prompt.nix {}
--- .zsh/git-prompt/git-prompt.nix
+++ .zsh/git-prompt/git-prompt.nix
@@ -1,12 +1,13 @@
-{ cabal, parsec, process }:
-
-cabal.mkDerivation (self: {
+{ mkDerivation, base, parsec, process, stdenv }:
+mkDerivation {
   pname = "git-prompt";
-  version = "0.0";
-  src = ./src/.;
+  version = "0.1.0.0";
+  src = ./.;
   isLibrary = false;
   isExecutable = true;
-  buildDepends = [
-    parsec process
-  ];
-})
+  buildDepends = [ base parsec process ];
+  homepage = "https://github.com/olivierverdier/zsh-git-prompt";
+  description = "Informative git prompt for zsh";
+  license = stdenv.lib.licenses.mit;
+}
+
