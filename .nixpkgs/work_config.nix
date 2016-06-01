e34f5ee512b2b64235387f841c4a211cb2a4ec34
Arseniy Seroka <ars.seroka@gmail.com>
2016-06-01 22:37:32 +0300

    bump

--- .nixpkgs/work_config.nix
+++ .nixpkgs/work_config.nix
@@ -3,7 +3,7 @@ pkgs : {
   /*allowBroken = true;*/
   packageOverrides = pkgs : with pkgs; rec {
       common = import ./common.nix { pkgs = pkgs; }; 
-      inherit (common)hugeEnv emacsEnv baseEnv develEnv steamEnv;
+      inherit (common) emacsEnv baseEnv develEnv steamEnv;
       workEnv = pkgs.buildEnv
       {
         name = "work-env";
