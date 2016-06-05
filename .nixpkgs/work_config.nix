859a9f6ca46ddc59b7de6b1aadab302062398786
Arseniy Seroka <ars.seroka@gmail.com>
2016-06-05 22:37:32 +0300

    bump

--- .nixpkgs/work_config.nix
+++ .nixpkgs/work_config.nix
@@ -3,7 +3,6 @@ pkgs : {
   /*allowBroken = true;*/
   packageOverrides = pkgs : with pkgs; rec {
       common = import ./common.nix { pkgs = pkgs; }; 
-      inherit (common) steamEnv;
       workEnv = pkgs.buildEnv
       {
         name = "work-env";
